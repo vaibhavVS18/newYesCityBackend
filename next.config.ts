5e3e639c89a622618f0daf399a00fee865e884e6
vaibhav <vaibhav.iiituna1111@gmail.com>
2025-08-27 09:46:43 +0530

    public first 5 cards, private others

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://new-yes-city-frontend.vercel.app',
+            value: 'http://localhost:3000',
           },
           {
             key: 'Access-Control-Allow-Methods',
