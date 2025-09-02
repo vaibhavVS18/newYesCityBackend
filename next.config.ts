767d68db7b5f35c55d0e35c8cced48139f78cea3
vaibhav <vaibhav.iiituna1111@gmail.com>
2025-09-02 22:46:55 +0530

    cookies on dev and prod

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'http://localhost:3000',
+            value: `${process.env.FRONTEND_URL}`,
           },
           {
             key: 'Access-Control-Allow-Methods',
